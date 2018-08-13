8d930faedb32b42cdf0b9cbf1de3135ff452bd12
Hrbáček Ivo <ivo.hrbacek@ixperta.com>
2018-08-13 10:57:34 +0200

    update

--- default_route_tracking.sh
+++ default_route_tracking.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -f
 #==============================================================================
-#title      :tracking script
+#title      :tracking script; author: Ivo Hrbacek
 #descrition :scrip trackimg default gw and changing redistribution of static default route to OSPF
 #author     :ivo.hrbacek@ixperta.com
-#version    :0.1
 #usage      :this script is executed via rc.local after reboot
-
+#version    :1.0
 #==============================================================================
 source /opt/CPshrd-R80/tmp/.CPprofile.sh
 
+# -----------------------------------------------------------
+# ISP
 # host to modify
 HOST=XXX
 
@@ -16,7 +17,9 @@ HOST=XXX
 COUNT=5
 
 #delay time
-SLEEP=10 
+SLEEP=10
+# ------------------------------------------------------------
+
 
 #log file
 LOG=/home/admin/scripts/default_gw_tracking.log
@@ -25,45 +28,52 @@ LOG=/home/admin/scripts/default_gw_tracking.log
 STATUS=0
 
 
+date >> $LOG
+echo "Default_gw_tracking.sh started" >> $LOG
+echo "Starting SLA loop with following parameters:" >> $LOG
+echo " SLA count value= $COUNT" >> $LOG
+echo " SLA sleep value= $SLEEP" >> $LOG
+
+
 # set route-redistribution to ospf2 from static-route default metric 50 on
 while true;
 do
-        
-        PING=$(ping -c $COUNT $HOST | grep received | cut -d ',' -f2 | cut -d ' ' -f2)
-                if [[ $PING -eq 0 ]]; then
-                        sleep $SLEEP
-                        PING02=$(ping -c $COUNT $HOST | grep received | cut -d ',' -f2 | cut -d ' ' -f2)
-                                if [[ $PING02 -eq 0 ]]; then
-                                        if [[ $STATUS -eq 0 ]]; then
-                                        date >> $LOG
-                                        echo "default gw not accessible, canceling redistribution of DGW into OSPF" >> $LOG
-                                        clish -c 'lock database override' -s >> $LOG 2>>$LOG
-                                        clish -c 'set route-redistribution to ospf2 off' -s >>$LOG 2>>$LOG
-                                        a=$?
-                                        if [[ "$a" -eq 1 ]];then
-                                                echo "configuration error" >> $LOG
-                                        else
-                                                echo "configuration successfull" >> $LOG
-                                        fi
-                                        STATUS=1
-                                        fi
-                                fi
-                else
-                        PING03=$(ping -c $COUNT $HOST | grep received | cut -d ',' -f2 | cut -d ' ' -f2)
- 
-                        if [[ $PING03 -gt 0 ]] && [[ $STATUS -eq 1 ]]  ; then
-                                date >> $LOG
-                                echo "default gw accessible,  installing redistribution of DGW into OSPF" >>$LOG 2>>$LOG
-                                clish -c 'lock database override' -s 2>>$LOG
-                                clish -c 'set route-redistribution to ospf2 from static-route default metric 50 on' -s >>$LOG 2>>$LOG
-                                a=$?
-                                if [[ "$a" -eq 1 ]];then
-                                        echo "configuration error" >> $LOG
-                                else
-                                        echo "configuration successfull" >> $LOG
-                                fi
-                                STATUS=0
+
+    PING01=$(ping -c $COUNT $HOST | grep received | cut -d ',' -f2 | cut -d ' ' -f2)
+        if [[ $PING01 -eq 0 ]]; then
+            sleep $SLEEP
+            PING02=$(ping -c $COUNT $HOST | grep received | cut -d ',' -f2 | cut -d ' ' -f2)
+                if [[ $PING02 -eq 0 ]]; then
+                    if [[ $STATUS -eq 0 ]]; then
+                        date >> $LOG
+                        echo "Default GW not accessible, canceling redistribution of DGW into OSPF" >> $LOG
+                        clish -c 'lock database override' -s >> $LOG 2>>$LOG
+                        clish -c 'set route-redistribution to ospf2 off' -s >>$LOG 2>>$LOG
+                        a=$?
+                        if [[ "$a" -eq 1 ]];then
+                            echo "Configuration error" >> $LOG
+                        else
+                            echo "Configuration successfull" >> $LOG
                         fi
+                    STATUS=1
+                    fi
                 fi
+        else
+            PING03=$(ping -c $COUNT $HOST | grep received | cut -d ',' -f2 | cut -d ' ' -f2)
+
+            if [[ $PING03 -gt 0 ]] && [[ $STATUS -eq 1 ]]  ; then
+                date >> $LOG
+                echo "Default GW accessible, installing redistribution of DGW into OSPF" >>$LOG 2>>$LOG
+                clish -c 'lock database override' -s 2>>$LOG
+                clish -c 'set route-redistribution to ospf2 from static-route default metric 50 on' -s >>$LOG 2>>$LOG
+                a=$?
+                if [[ "$a" -eq 1 ]];then
+                    echo "Configuration error" >> $LOG
+                else
+                    echo "Configuration successfull" >> $LOG
+                fi
+                STATUS=0
+            fi
+        fi
 sleep $SLEEP
 done
