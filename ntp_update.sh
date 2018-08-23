eb8442f6d534502d0e3d15db64f22007ea6d0102
Hrbáček Ivo <ivo.hrbacek@ixperta.com>
2018-08-23 09:36:39 +0200

    update

---
+++ ntp_update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -f
+#
+#==============================================================================
+#title      :first time generator for POC help sritpt for loading basic config data
+#author     :ivo.hrbacek@ixperta.com a laura
+#version    :0.0001
+# CP enviroment variables for cron see sk77300, sk90441
+source /opt/CPshrd-R80/tmp/.CPprofile.sh
+
+SLEEP=60
+
+while true;
+do
+
+/sbin/ntpdate tik.cesnet.cz
+a=$?
+    if [[ "$a" -eq 1 ]];then
+	   /sbin/ntpdate tak.cesnet.cz			             
+    fi
+    
+sleep $SLEEP
+done
